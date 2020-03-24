cb12e4e9c6b00971a14d92b7e7f4b13452ef47db
Harrison Leach <HarrisonL7190@gmail.com>
2020-03-25 11:48:49 +1300

    Remove topic selection from post submission
    Not needed for now, as it does not add significant value.
    Also would require admins to set the topics

--- app/src/pages/SubmitPage.tsx
+++ app/src/pages/SubmitPage.tsx
@@ -20,33 +20,14 @@ import React, { useState } from 'react';
 const channelInterfaceOptions = {
   header: 'Channel',
   subHeader: 'Select the channel',
-  message: 'Only users in the selected channel will see the post',
-};
-
-const topicInterfaceOptions = {
-  header: 'Topic',
-  subHeader: 'Select the topic',
+  message: 'The post will be found in the selected channel',
 };
 
 // TODO: Replace with apollo hooks useQuery once back-end is setup
 const channels = ['Engineering', 'Commerce', 'Science', 'Law', 'Arts'];
-const topics = [
-  'Coronavirus at university',
-  'Working from home',
-  'Self-isolation zoom parties',
-  'Depression',
-  'Idiots in lectures',
-  'Lecturers',
-  'Engineering Careers',
-  'Commerce Careers',
-  'Science Careers',
-  'Law Careers',
-  'McDonalds Careers',
-];
 
 const SubmitPage: React.FC<{}> = () => {
   const [selectedChannel, setSelectedChannel] = useState<string>();
-  const [selectedTopic, setSelectedTopic] = useState<string>();
   const [title, setTitle] = useState<string>();
   const [confessionText, setConfessionText] = useState<string>();
   const [author, setAuthor] = useState<string>();
@@ -59,7 +40,12 @@ const SubmitPage: React.FC<{}> = () => {
             <IonBackButton defaultHref="/page/feed" text="Cancel" />
           </IonButtons>
           <IonButtons slot="primary">
-            <IonButton disabled={true}>Post</IonButton>
+            <IonButton
+              disabled={!(selectedChannel && title && confessionText && author)}
+              href="/"
+            >
+              Post
+            </IonButton>
           </IonButtons>
           <IonTitle>Create Post</IonTitle>
         </IonToolbar>
@@ -80,24 +66,6 @@ const SubmitPage: React.FC<{}> = () => {
             >
               {channels.map((channel, index) => (
                 <IonSelectOption key={index}>{channel}</IonSelectOption>
-              ))}
-            </IonSelect>
-          </IonItem>
-
-          <IonItem>
-            <IonLabel style={selectedTopic ? {} : { color: 'lightgrey' }}>
-              {selectedTopic || 'Choose a topic'}
-            </IonLabel>
-            <IonSelect
-              selectedText={' '}
-              interfaceOptions={topicInterfaceOptions}
-              interface="popover"
-              multiple={false}
-              onIonChange={(e) => setSelectedTopic(e.detail.value)}
-              value={selectedTopic}
-            >
-              {topics.map((topic, index) => (
-                <IonSelectOption key={index}>{topic}</IonSelectOption>
               ))}
             </IonSelect>
           </IonItem>
@@ -127,14 +95,13 @@ const SubmitPage: React.FC<{}> = () => {
             />
           </IonItem>
           <IonItem>
-            <IonLabel position="stacked">Author Name</IonLabel>
+            <IonLabel position="stacked">Author (Optional)</IonLabel>
             <IonInput
               clearInput={true}
               inputMode="text"
               maxlength={255}
               minlength={2}
-              placeholder="e.g. 'anonymous' or your name"
-              required={true}
+              placeholder="e.g. 'Depressed engineer' or 'Lonely arts student'"
               onIonChange={(e) => setAuthor(e.detail.value!)}
               value={author}
             />
