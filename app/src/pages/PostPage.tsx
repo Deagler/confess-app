0ef6085c6c3276e7a2bf072bd237ce60bb337218
Maggie Liu <maggie.liu@figured.com>
2020-03-27 14:59:39 +1300

    Add back button and title of post page

--- app/src/pages/PostPage.tsx
+++ app/src/pages/PostPage.tsx
@@ -1,12 +1,49 @@
 import React, { useState } from 'react';
-import { IonPage } from '@ionic/react';
+import {
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonTitle,
+  IonMenuButton,
+  IonButton,
+  IonRow,
+} from '@ionic/react';
 
 import Post, { PostProps } from '../components/Post';
 import { useParams, Route } from 'react-router-dom';
 
 const Postpage: React.FC = () => {
   const { id } = useParams();
-  return <IonPage>{id}</IonPage>;
+  const testPosts: PostProps[] = [
+    {
+      id: 1,
+      title: 'This is the title',
+      date: new Date(),
+      content: 'This is the content',
+      author: 'This is the author',
+    },
+  ];
+
+  const [posts, setPosts] = useState(testPosts);
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar className="ion-justify-content-center">
+          <IonRow>
+            <IonButton className="ion-margin">Back</IonButton>
+            <IonTitle>Post</IonTitle>
+          </IonRow>
+        </IonToolbar>
+      </IonHeader>{' '}
+      <IonContent>
+        {posts.map((post: PostProps, i: number) => (
+          <Post key={i} {...post} />
+        ))}
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Postpage;
