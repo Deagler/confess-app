3e3d32c963affafccb9f0efca4018da8a6790964
Sukhans Asrani <sukhansnz@gmail.com>
2020-03-28 19:15:32 +1300

    Add .env to frontend

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import urljoin from 'url-join';
+console.log(urljoin(process.env.REACT_APP_ENDPOINT_URL!, 'graph'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
