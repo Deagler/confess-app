97027445d42dc99989c213883309f8ff9d98d269
Sukhans Asrani <sukhansnz@gmail.com>
2020-03-29 01:59:03 +1300

    Fix Tests Attempt #2

--- app/src/pages/AdminPage.test.tsx
+++ app/src/pages/AdminPage.test.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import AdminPage from './FeedPage';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { apolloClient } from '../services/api/apolloClient';
 
 test('renders without crashing', () => {
-  const { baseElement } = render(<AdminPage />);
+  const tree = (
+    <ApolloProvider client={apolloClient}>
+      <AdminPage />
+    </ApolloProvider>
+  );
+
+  const { baseElement } = render(tree);
   expect(baseElement).toBeDefined();
 });
--- app/src/pages/FeedPage.test.tsx
+++ app/src/pages/FeedPage.test.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import FeedPage from './FeedPage';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { apolloClient } from '../services/api/apolloClient';
 
 test('renders without crashing', () => {
-  const { baseElement } = render(<FeedPage />);
+  const tree = (
+    <ApolloProvider client={apolloClient}>
+      <FeedPage />
+    </ApolloProvider>
+  );
+  const { baseElement } = render(tree);
   expect(baseElement).toBeDefined();
 });
