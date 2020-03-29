328cd99859b72d700e40f38f24980e1b58dcc610
Harrison Leach <HarrisonL7190@gmail.com>
2020-03-30 09:03:01 +1300

    Update unit tests

--- app/src/components/Menu.test.tsx
+++ app/src/components/Menu.test.tsx
@@ -2,21 +2,27 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Menu from './Menu';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { apolloClient } from '../services/api/apolloClient';
 
 test('renders without crashing', () => {
   const { baseElement } = render(
-    <MemoryRouter>
-      <Menu selectedPage="feed" />
-    </MemoryRouter>
+    <ApolloProvider client={apolloClient}>
+      <MemoryRouter>
+        <Menu selectedPage="feed" />
+      </MemoryRouter>
+    </ApolloProvider>
   );
   expect(baseElement).toBeDefined();
 });
 
 test('displays content properly', async () => {
   const { findByText } = render(
-    <MemoryRouter>
-      <Menu selectedPage="feed" />
-    </MemoryRouter>
+    <ApolloProvider client={apolloClient}>
+      <MemoryRouter>
+        <Menu selectedPage="feed" />
+      </MemoryRouter>
+    </ApolloProvider>
   );
   await findByText('Confess');
   await findByText('LogIn');
