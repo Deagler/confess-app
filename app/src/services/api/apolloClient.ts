2ad5a649022e24cb98e6c29379dc37f48fa3965a
Sukhans Asrani <sukhansnz@gmail.com>
2020-03-29 17:37:26 +1300

    Remove .env from /app as well

--- app/src/services/api/apolloClient.ts
+++ app/src/services/api/apolloClient.ts
@@ -2,5 +2,5 @@ import ApolloClient from 'apollo-boost';
 import urljoin from 'url-join';
 
 export const apolloClient = new ApolloClient({
-  uri: urljoin(process.env.REACT_APP_ENDPOINT_URL!, 'graph'),
+  uri: urljoin(process.env.REACT_APP_ENDPOINT_URL || '', 'graph'),
 });
