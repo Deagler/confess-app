2a13d3a0867589e1143f1e1c1a117ae0135f179e
Harrison Leach <HarrisonL7190@gmail.com>
2020-04-01 17:17:51 +1300

    Add submit comment button
    Gives users are better indication of how to submit
    comments

--- app/src/components/NewCommentInput.tsx
+++ app/src/components/NewCommentInput.tsx
@@ -1,5 +1,17 @@
-import { IonCard, IonTextarea, IonCardContent, IonItem } from '@ionic/react';
+import {
+  IonCard,
+  IonTextarea,
+  IonCardContent,
+  IonItem,
+  IonGrid,
+  IonRow,
+  IonButton,
+  IonSpinner,
+  IonIcon,
+  IonCol,
+} from '@ionic/react';
 import React, { useState, RefObject } from 'react';
+import { send } from 'ionicons/icons';
 
 export interface NewCommentInputProps {
   onSubmitComment: (newCommentContent: string) => void;
@@ -11,6 +23,7 @@ const NewCommentInput: React.FC<NewCommentInputProps> = ({
   inputRef,
 }) => {
   const [text, setText] = useState<string>();
+  const [loading, setLoading] = useState(false);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLIonTextareaElement>) => {
     if (e.keyCode === 13 && e.shiftKey === false && text) {
@@ -23,18 +36,43 @@ const NewCommentInput: React.FC<NewCommentInputProps> = ({
   return (
     <IonCard>
       <IonCardContent>
-        <IonItem>
-          <IonTextarea
-            placeholder="Write a comment..."
-            value={text}
-            onKeyDown={handleKeyDown}
-            onIonChange={(e) => {
-              setText(e.detail.value!);
-            }}
-            autofocus={true}
-            ref={inputRef}
-          />
-        </IonItem>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="12">
+              <IonItem>
+                <IonTextarea
+                  placeholder="Write a comment..."
+                  value={text}
+                  onKeyDown={handleKeyDown}
+                  onIonChange={(e) => {
+                    setText(e.detail.value!);
+                  }}
+                  autofocus={true}
+                  rows={6}
+                  ref={inputRef}
+                />
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol style={{ padding: '0px' }} size="12">
+              <div className="ion-text-end">
+                <IonButton
+                  size="small"
+                  disabled={false}
+                  onClick={() => setLoading(!loading)}
+                >
+                  {(loading && <IonSpinner />) || (
+                    <>
+                      {'Submit   '}
+                      <IonIcon size="small" slot="icon-only" icon={send} />
+                    </>
+                  )}
+                </IonButton>
+              </div>{' '}
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonCardContent>
     </IonCard>
   );
