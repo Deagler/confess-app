bfd02471c46b77dcc5ef59ddf1466f35a7ad7cb7
Harrison Leach <HarrisonL7190@gmail.com>
2020-04-01 19:41:56 +1300

    Update unit tests for post page

--- app/src/pages/PostPage.test.tsx
+++ app/src/pages/PostPage.test.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import PostPage from './PostPage';
 import { MemoryRouter } from 'react-router-dom';
+import { wrapWithApolloProvider, wrapWithRouter } from '../utils/testing';
 
 test('renders without crashing', () => {
   const { baseElement } = render(
-    <MemoryRouter>
-      <PostPage />
-    </MemoryRouter>
+    wrapWithApolloProvider(wrapWithRouter(<PostPage />))
   );
   expect(baseElement).toBeDefined();
 });
