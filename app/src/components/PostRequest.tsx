2a2ceba28efc19376faa197f347078305db9b0c1
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-04-02 18:04:31 +1300

    Integrate post approval with admin page

--- app/src/components/PostRequest.tsx
+++ app/src/components/PostRequest.tsx
@@ -10,11 +10,17 @@ import {
   IonCardTitle,
   IonCardSubtitle,
   IonCardContent,
+  IonToast,
+  IonSpinner,
 } from '@ionic/react';
 import moment from 'moment';
+import { useMutation } from '@apollo/react-hooks';
+
+import { APPROVE_POST } from '../common/graphql/admin';
+import { ApprovePost, ApprovePostVariables } from '../types/ApprovePost';
 
 export interface PostRequestProps {
-  id: number;
+  id: string;
   title: string;
   date: Date;
   content: string;
@@ -24,34 +30,57 @@ export interface PostRequestProps {
 const PostRequest: React.FC<PostRequestProps> = (props: PostRequestProps) => {
   const { id, title, date, content, author } = props;
 
+  const [approvePost, { loading, error }] = useMutation<
+    ApprovePost,
+    ApprovePostVariables
+  >(APPROVE_POST);
+
+  const approveHandler = async () => {
+    await approvePost({
+      variables: {
+        postId: id,
+        communityId: 'HW6lY4kJOpqSpL39hbUV',
+        approverId: 'aVyC8BFy1f5qGzXVwGSu',
+      },
+    });
+  };
+
   return (
-    <IonCard>
-      <IonCardHeader>
-        <IonCardSubtitle>{`#${id}`}</IonCardSubtitle>
-        <IonCardTitle>{title}</IonCardTitle>
-        <IonCardSubtitle>{moment(date).fromNow()}</IonCardSubtitle>
-      </IonCardHeader>
+    <>
+      <IonToast isOpen={!!error} message={error?.message} duration={2000} />
+      <IonCard>
+        <IonCardHeader>
+          <IonCardSubtitle>{`#${id}`}</IonCardSubtitle>
+          <IonCardTitle>{title}</IonCardTitle>
+          <IonCardSubtitle>{moment(date).fromNow()}</IonCardSubtitle>
+        </IonCardHeader>
 
-      <IonCardContent>{content}</IonCardContent>
-      <IonCardContent>{author || 'Anonymous'}</IonCardContent>
+        <IonCardContent>{content}</IonCardContent>
+        <IonCardContent>{author || 'Anonymous'}</IonCardContent>
 
-      <IonItemDivider />
+        <IonItemDivider />
 
-      <IonGrid>
-        <IonRow className="ion-justify-content-center">
-          <IonCol>
-            <IonButton color="primary" fill="outline" expand="block">
-              Approve
-            </IonButton>
-          </IonCol>
-          <IonCol>
-            <IonButton color="danger" fill="outline" expand="block">
-              Deny
-            </IonButton>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
-    </IonCard>
+        <IonGrid>
+          <IonRow className="ion-justify-content-center">
+            <IonCol>
+              <IonButton
+                color="primary"
+                fill="outline"
+                expand="block"
+                onClick={approveHandler}
+              >
+                {loading ? <IonSpinner /> : 'Approve'}
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton color="danger" fill="outline" expand="block">
+                Deny
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonCard>
+    </>
   );
 };
 
