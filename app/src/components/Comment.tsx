7211c3ccaca1b39b5f60d04dffaa0d2ae9d7751c
Maggie Liu <maggieqliu81@gmail.com>
2020-04-05 22:22:07 +1200

    Add update mutation to comment component, wip

--- app/src/components/Comment.tsx
+++ app/src/components/Comment.tsx
@@ -11,7 +11,10 @@ import React, { useState } from 'react';
 import { timeOutline, heart, chatbox, shareSocial } from 'ionicons/icons';
 import moment from 'moment';
 import './Comment.css';
-
+import { useMutation, useQuery } from '@apollo/react-hooks';
+import { GET_LOCAL_USER } from '../common/graphql/localState';
+import { GetLocalUser } from '../types/GetLocalUser';
+import { SERVER_TOGGLE_LIKE_COMMENT } from '../common/graphql/comments';
 interface CommunityData {
   abbreviation: string;
 }
@@ -24,29 +27,72 @@ export interface UserData {
 }
 
 export interface CommentData {
+  id: string;
   creationTimestamp: number;
   // TODO: Update once user resolvers are written
   author?: UserData | null;
   content: string;
   totalLikes: number;
   likes?: (UserData | null)[];
+  isCommentLikedByUser: boolean | null;
 }
 
 export interface CommentProps extends CommentData {
   onReply: (author: string) => void;
+  isCommentLikedByUser;
 }
 
 const Comment: React.FC<CommentProps> = (props: CommentProps) => {
-  const { content, author, onReply, creationTimestamp, totalLikes } = props;
+  const {
+    id,
+    content,
+    author,
+    onReply,
+    creationTimestamp,
+    totalLikes,
+    isCommentLikedByUser,
+  } = props;
   const authorDisplayName = author
     ? `${author.firstName} ${author.lastName} (${author.communityUsername})`
     : 'unknown';
   const authorCommunity = author?.community?.abbreviation ?? '';
-
+  const localUserQuery = useQuery<GetLocalUser>(GET_LOCAL_USER, {
+    fetchPolicy: 'network-only',
+  });
+  const userLoggedIn = !!localUserQuery.data?.localUser;
   // TODO: Add liking mutation, and fetch liked status
   const [liked, setLiked] = useState<boolean>(false);
   const likedButtonColor: string = liked ? 'primary' : 'medium';
+  const [serverToggleLike, serverLikeInfo] = useMutation(
+    SERVER_TOGGLE_LIKE_COMMENT
+  );
+  const handleLikeButtonClick = async (communityId, postId, commentId) => {
+    if (serverLikeInfo.loading) {
+      return;
+    }
 
+    await serverToggleLike({
+      variables: {
+        communityId: 'HW6lY4kJOpqSpL39hbUV',
+        postId,
+        commentId,
+      },
+      optimisticResponse: {
+        toggleLikeComment: {
+          code: 200,
+          message: 'Comment liked.',
+          success: true,
+          comment: {
+            id,
+            isCommentLikedByUser: isCommentLikedByUser!,
+            totalLikes: isCommentLikedByUser ? totalLikes - 1 : totalLikes + 1,
+            __typename: 'Comment',
+          },
+          __typename: 'CommentUpdatedResponse',
+        },
+      },
+    });
+  };
   return (
     <IonItem>
       <IonGrid>
@@ -78,10 +124,13 @@ const Comment: React.FC<CommentProps> = (props: CommentProps) => {
           <IonCol>
             <IonItem lines="none">
               <IonButton
+                onClick={() =>
+                  handleLikeButtonClick('HW6lY4kJOpqSpL39hbUV', '', id)
+                } // GET POST ID
                 fill="clear"
                 expand="full"
-                color={likedButtonColor}
-                onClick={() => setLiked(!liked)}
+                disabled={!userLoggedIn || serverLikeInfo.loading}
+                color={isCommentLikedByUser ? 'danger' : 'primary'}
               >
                 <IonIcon color={likedButtonColor} icon={heart} />
                 <IonLabel>{totalLikes}</IonLabel>
