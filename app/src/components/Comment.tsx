ffed1b3992fe3f6f0fc9940ae0d84bd7c2b39f75
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-04-09 16:23:08 +1200

    Remove share button from comments

--- app/src/components/Comment.tsx
+++ app/src/components/Comment.tsx
@@ -112,7 +112,7 @@ const Comment: React.FC<CommentProps> = (props: CommentProps) => {
           <IonRow>
             <IonCol size="12" size-sm="6">
               <IonItem lines="none">
-                <IonLabel slot="start">
+                <IonLabel>
                   <h6>
                     {authorDisplayName} <span>&middot;</span> {authorCommunity}
                   </h6>
@@ -172,9 +172,6 @@ const Comment: React.FC<CommentProps> = (props: CommentProps) => {
                     <IonIcon color="medium" icon={chatbox} />
                   </IonButton>
                 </LoginTooltip>
-                <IonButton fill="clear" expand="full" color="medium">
-                  <IonIcon icon={shareSocial} />
-                </IonButton>
               </IonItem>
             </IonCol>
           </IonRow>
