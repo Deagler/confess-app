a668b3d2d7b80165605ce07db01b8f835a1bb6a2
Harrison Leach <HarrisonL7190@gmail.com>
2020-04-16 17:02:56 +1200

    Update community links on landing page to be more pronounced

--- app/src/components/CommunityList.tsx
+++ app/src/components/CommunityList.tsx
@@ -6,14 +6,26 @@ import {
   IonLabel,
   IonSpinner,
   IonToast,
+  IonCard,
 } from '@ionic/react';
-import { school } from 'ionicons/icons';
+import { school, arrowForwardOutline } from 'ionicons/icons';
 import { backgroundColor } from '../theme/global';
 import { GET_COMMUNITIES } from '../common/graphql/communities';
 import { GetCommunities } from '../types/GetCommunities';
 import { useQuery } from '@apollo/react-hooks';
 import { buildLink } from '../utils';
 import { useHistory } from 'react-router';
+import { css } from 'glamor';
+
+const moreCommunities = css({
+  marginInlineStart: '10px',
+  marginInlineEnd: '10px',
+  borderStyle: 'dashed',
+  borderWidth: '2px',
+  borderColor: 'lightgrey',
+  borderRadius: '7px',
+  textAlign: 'center',
+});
 
 const CommunityList: React.FC<{}> = () => {
   const { data, loading, error } = useQuery<GetCommunities>(GET_COMMUNITIES);
@@ -37,24 +49,32 @@ const CommunityList: React.FC<{}> = () => {
         ) : (
           communities &&
           communities.map((community, i) => (
-            <IonItem
-              button={true}
-              key={i}
-              {...backgroundColor}
-              onClick={() => handleCommunityChange(community?.id)}
-            >
-              <IonAvatar slot="start">
-                <img src={community?.imageURI} alt={community?.name} />
-              </IonAvatar>
-              <IonLabel>{community?.name}</IonLabel>
-            </IonItem>
+            <IonCard className="ion-margin-bottom" key={i}>
+              <IonItem
+                lines="none"
+                button={true}
+                {...backgroundColor}
+                onClick={() => handleCommunityChange(community?.id)}
+                detail={true}
+                detailIcon={arrowForwardOutline}
+              >
+                <IonAvatar slot="start">
+                  <img src={community?.imageURI} alt={community?.name} />
+                </IonAvatar>
+                <IonLabel className="ion-text-center">
+                  {community?.name}
+                </IonLabel>
+              </IonItem>
+            </IonCard>
           ))
         )}
-        <IonItem {...backgroundColor}>
+        <IonItem {...backgroundColor} {...moreCommunities} lines="none">
           <IonAvatar slot="start">
-            <img alt="Other" src={school} />
+            <img style={{ filter: 'opacity(30%)' }} alt="Other" src={school} />
           </IonAvatar>
-          <IonLabel>More universities coming soon.</IonLabel>
+          <IonLabel style={{ color: '#818181' }}>
+            More universities coming soon.
+          </IonLabel>
         </IonItem>
       </IonList>
     </>
