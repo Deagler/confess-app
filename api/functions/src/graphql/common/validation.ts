948a2d2ad0f5c8c75f375276f09c6cb70b300d77
Maggie Liu <maggieqliu81@gmail.com>
2020-04-23 15:11:11 +1200

    Add validation to display name

---
+++ api/functions/src/graphql/common/validation.ts
@@ -0,0 +1,41 @@
+import { UserInputError } from 'apollo-server-express';
+import { firebaseApp } from '../../firebase';
+
+const usersCollection = firebaseApp.firestore().collection('users');
+/**
+ * @param displayName
+ * Display name has to be at least 3 characters and can't exceed 24 characters
+ * Has to be alphanumeric
+ * has to be unique
+ * has to contain at least one letter, can not be only integers
+ */
+export async function validateDisplayName(displayName) {
+  const displayNameRemoveSpace = displayName.replace(/ \t\n\r\0\x0B/g, '');
+  if (
+    !displayNameRemoveSpace ||
+    !displayNameRemoveSpace.trim() ||
+    !displayNameRemoveSpace.match(/^[0-9a-zA-Z_.-]+$/)
+  ) {
+    throw new UserInputError('Invalid display name');
+  } else if (!(await IsDisplayNameUnique(displayNameRemoveSpace))) {
+    throw new UserInputError('This display name is not available');
+  } else if (
+    displayNameRemoveSpace.length > 24 ||
+    displayNameRemoveSpace.length < 3
+  ) {
+    throw new UserInputError(
+      'The length of display has to be with in 3 to 24 characters'
+    );
+  } else if (!/[a-zA-Z]/.test(displayNameRemoveSpace)) {
+    throw new UserInputError(
+      'The display name has to contain at least one letter'
+    );
+  }
+}
+
+async function IsDisplayNameUnique(displayName) {
+  const userDocs = await usersCollection
+    .where('displayNameId', '==', displayName.toLowerCase())
+    .get();
+  return userDocs.empty;
+}
